8165256cdaecf4a02ceed39ff70f5ecdfdfb5e92
ivansukhoparov <rbcdaaa@gmail.com>
2024-06-17 22:19:29 +0200

    delete old

--- bloggs/src/app/components/Header/NavMenu/NavItem/NavItem.tsx
+++
@@ -1,8 +0,0 @@
-import s from "./NavItem.module.css"
-import {NavLink} from "react-router-dom";
-
-export default function NavItem(props:any) {
-    return (
-        <NavLink to={props.link} className={s.item}>{props.name}</NavLink>
-    )
-}
